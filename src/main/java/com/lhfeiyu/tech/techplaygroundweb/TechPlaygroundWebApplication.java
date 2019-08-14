8951e539d9fd79ea7b906242bccabe5fdfe7ece8
yurh <airson_yu@163.com>
2019-08-14 11:27:15 +0800

    init

---
+++ src/main/java/com/lhfeiyu/tech/techplaygroundweb/TechPlaygroundWebApplication.java
@@ -0,0 +1,13 @@
+package com.lhfeiyu.tech.techplaygroundweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechPlaygroundWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechPlaygroundWebApplication.class, args);
+    }
+
+}
